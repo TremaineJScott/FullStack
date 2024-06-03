f1793aafef66c28cf641d5f1b23edd035a896d9f
Scott Tremaine <j.scott.tremaine@gmail.com>
2024-06-03 19:46:08 -0400

    completed components

--- full-stack-app/src/app/components/pet-list/pet-list.component.ts
+++ full-stack-app/src/app/components/pet-list/pet-list.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterLinkActive, RouterModule } from '@angular/router';
+import { PetService } from '../../services/pet.service';
+import { Pet } from '../../models/pet.model';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pet-list',
-  standalone: true,
-  imports: [],
   templateUrl: './pet-list.component.html',
-  styleUrl: './pet-list.component.css'
+  styleUrls: ['./pet-list.component.css'],
+  standalone: true,
+  imports: [FormsModule, CommonModule, RouterLinkActive, RouterModule]
 })
-export class PetListComponent {
+export class PetListComponent implements OnInit {
+  pets: Pet[] = [];
+  newPet: Pet = {
+    petId: 0,
+    name: '',
+    type: '',
+    breed: '',
+    age: 0,
+    description: ''
+  };
 
+  constructor(private petService: PetService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.pets = this.petService.getPets();
+  }
+
+  addPet(): void {
+    if (this.newPet.name && this.newPet.type && this.newPet.breed && this.newPet.age > 0 && this.newPet.description) {
+      this.petService.addPet(this.newPet);
+      this.newPet = {
+        petId: 0,
+        name: '',
+        type: '',
+        breed: '',
+        age: 0,
+        description: ''
+      };
+    }
+  }
 }
+
